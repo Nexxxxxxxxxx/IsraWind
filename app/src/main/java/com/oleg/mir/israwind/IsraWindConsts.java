5333cf163e47b370f76a8740db4d6e5201b91303
Nexx <oleg.mir@gmail.com>
2019-01-05 00:42:40 +0200

    new class for constants

---
+++ app/src/main/java/com/oleg/mir/israwind/IsraWindConsts.java
@@ -0,0 +1,5 @@
+package com.oleg.mir.israwind;
+
+public final class IsraWindConsts {
+    public static final String[] Location = { "Haifa - Studentim", "Kineret", "Eilat", "Hertzeliya - Sidni Ali" };
+}
