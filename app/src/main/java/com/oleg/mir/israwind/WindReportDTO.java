0b62ce93d981decea1112a34037e9f7cf75fc01c
Nexx <oleg.mir@gmail.com>
2019-01-05 00:26:03 +0200

    added reportTime

--- app/src/main/java/com/oleg/mir/israwind/WindReportDTO.java
+++ app/src/main/java/com/oleg/mir/israwind/WindReportDTO.java
@@ -1,10 +1,16 @@
 package com.oleg.mir.israwind;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class WindReportDTO {
     private static final String type="WindReportDTO";
     public int windSpeed;
     public int gustSpeed;
     public String comment;
+    public String reportTime;
     public enum WindDirection {
         N,
         S,
@@ -14,10 +20,21 @@ public class WindReportDTO {
 
     WindDirection windDirection;
 
-    WindReportDTO(int _windSpeed, WindDirection _windDirection)
+    WindReportDTO(int _windSpeed, WindDirection _windDirection )
     {
         windSpeed = _windSpeed;
         windDirection = _windDirection;
+        reportTime = GetCurrentDateTime();
+        comment = "test comment";
+    }
+
+    private String GetCurrentDateTime()
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy HH:mm");
+        sdf.setTimeZone(TimeZone.getTimeZone("Israel"));
+        String currentDateandTime = sdf.format(new Date());
+
+        return currentDateandTime;
     }
 }
 
