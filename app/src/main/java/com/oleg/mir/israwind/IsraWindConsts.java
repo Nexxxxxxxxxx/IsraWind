ade7e496089911275bc0b35b8631fcaddeefcd81
Nexx <oleg.mir@gmail.com>
2019-01-09 23:00:52 +0200

    Update IsraWindConsts.java

--- app/src/main/java/com/oleg/mir/israwind/IsraWindConsts.java
+++ app/src/main/java/com/oleg/mir/israwind/IsraWindConsts.java
@@ -1,7 +1,7 @@
 package com.oleg.mir.israwind;
 
 public final class IsraWindConsts {
-    public static final String[] Location = {"Kineret", "Eilat", "Betzet", "Akko", "Kiryat Yam", "Kiryat Haim", "Haifa - Bat Galim", "Haifa - Studentim","Atlit","Sdot Yam","Beit Yanai","Maayan Zvi","Kenetiki", "Hertzeliya - Sidni Ali","Hertzeliya - Maaliyot","Hof a Tzuk","Bat Yam", "Ashdod","Ashkelon" };
-    public static final String[] WindDirections = {"N", "S", "W", "E", "NW", "NE", "SE", "SW"};
+    public static final String[] Location = {"Kineret - Diamond", "Eilat", "Betzet", "Akko", "Kiryat Yam", "Kiryat Haim", "Haifa - Bat Galim", "Haifa - Studentim","Atlit","Sdot Yam","Beit Yanai","Maayan Zvi","Kenetiki", "Hertzeliya - Sidni Ali","Hertzeliya - Maaliyot","Hof a Tzuk","Bat Yam", "Ashdod","Ashkelon" };
+    public static final String[] WindDirections = {"N ↓", "S ↑", "W →", "E ←", "NW ↘", "NE ↙", "SE ↖", "SW ↗"};
     public static final String LastWindReportReference = "LastWindReport";
 }
