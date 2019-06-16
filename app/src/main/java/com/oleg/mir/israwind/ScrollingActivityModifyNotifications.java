4175e7f1337a39deb77f5bb5848627c50d8a0cd3
Nexx <oleg.mir@gmail.com>
2019-06-16 22:12:19 +0300

    notifications writing to local file

--- app/src/main/java/com/oleg/mir/israwind/ScrollingActivityModifyNotifications.java
+++ app/src/main/java/com/oleg/mir/israwind/ScrollingActivityModifyNotifications.java
@@ -17,28 +17,28 @@ import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessaging;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.StreamCorruptedException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ScrollingActivityModifyNotifications extends AppCompatActivity {
 
-    DatabaseReference locationRef;
-    Map<String,String> allNotifications;
     String locationNotification;
-    Map<String,Object> taskMap;
     String locationId;
+
+    private String subFolder = "/userdata";
+    private String file = "user_notifications.ser";
+    Map<String, Object> userSettings = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,29 +53,109 @@ public class ScrollingActivityModifyNotifications extends AppCompatActivity {
             return;
         }
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference windReportDatabase = database.getReference(IsraWindConsts.UsersInfoReference);
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        FirebaseUser user = auth.getCurrentUser();
-
-        locationRef = windReportDatabase.child(user.getUid()).child("notificationSettings");
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        locationRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                allNotifications = (HashMap)dataSnapshot.getValue();
+        readSetttings();
+        ShowNotificationSettings();
+    }
 
-                ShowNotificationSettings();
+    public void writeSettings() {
+        File cacheDir = null;
+        File appDirectory = null;
+
+        if (android.os.Environment.getExternalStorageState().
+                equals(android.os.Environment.MEDIA_MOUNTED)) {
+            cacheDir = getApplicationContext().getExternalCacheDir();
+            appDirectory = new File(cacheDir + subFolder);
+
+        } else {
+            cacheDir = getApplicationContext().getCacheDir();
+            String BaseFolder = cacheDir.getAbsolutePath();
+            appDirectory = new File(BaseFolder + subFolder);
+
+        }
+
+        if (appDirectory != null && !appDirectory.exists()) {
+            appDirectory.mkdirs();
+        }
+
+        File fileName = new File(appDirectory, file);
+
+        FileOutputStream fos = null;
+        ObjectOutputStream out = null;
+        try {
+            fos = new FileOutputStream(fileName);
+            out = new ObjectOutputStream(fos);
+            out.writeObject(userSettings);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fos != null)
+                    fos.flush();
+                fos.close();
+                if (out != null)
+                    out.flush();
+                out.close();
+            } catch (Exception e) {
+
             }
+        }
+    }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
 
+    public void readSetttings() {
+        File cacheDir = null;
+        File appDirectory = null;
+        if (android.os.Environment.getExternalStorageState().
+                equals(android.os.Environment.MEDIA_MOUNTED)) {
+            cacheDir = getApplicationContext().getExternalCacheDir();
+            appDirectory = new File(cacheDir + subFolder);
+        } else {
+            cacheDir = getApplicationContext().getCacheDir();
+            String BaseFolder = cacheDir.getAbsolutePath();
+            appDirectory = new File(BaseFolder + subFolder);
+        }
+
+        if (appDirectory != null && !appDirectory.exists()) return; // File does not exist
+
+        File fileName = new File(appDirectory, file);
+
+        FileInputStream fis = null;
+        ObjectInputStream in = null;
+        try {
+            fis = new FileInputStream(fileName);
+            in = new ObjectInputStream(fis);
+            Map<String, Object> myHashMap = (Map<String, Object> ) in.readObject();
+            userSettings = myHashMap;
+            System.out.println("count of hash map::"+userSettings.size() + " " + userSettings);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (StreamCorruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+
+            try {
+                if(fis != null) {
+                    fis.close();
+                }
+                if(in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        });
+        }
     }
 
     private boolean StringToBoolean(String s)
@@ -108,9 +188,6 @@ public class ScrollingActivityModifyNotifications extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                     }
                 })
-
-                // A null listener allows the button to dismiss the dialog and take no further action.
-                //.setNegativeButton(android.R.string.no, null)
                 .setIcon(android.R.drawable.stat_notify_error)
                 .show();
     }
@@ -127,14 +204,14 @@ public class ScrollingActivityModifyNotifications extends AppCompatActivity {
             sw.setText(locationText);
             sw.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
 
-            if(allNotifications == null)
+            if(userSettings == null)
             {
                 sw.setChecked(false);
             }
             else
             {
                 Log.d("myTag", "location: "+ locationText);
-                Object b=allNotifications.get(locationText);
+                Object b=userSettings.get(locationText);
                 if(b == null)
                 {
                     sw.setChecked(false);
@@ -146,11 +223,10 @@ public class ScrollingActivityModifyNotifications extends AppCompatActivity {
 
             }
 
-
             NestedScrollView scrollView = (NestedScrollView) findViewById(R.id.ScrollViewID);
 
             LinearLayout linearLayout = findViewById(R.id.TogglesLinearLayoutID);
-            // Add Switch to LinearLayout
+
             if (linearLayout != null) {
                 linearLayout.addView(sw);
             }
@@ -160,10 +236,11 @@ public class ScrollingActivityModifyNotifications extends AppCompatActivity {
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                     locationNotification = buttonView.getText().toString();
 
-                    taskMap = new HashMap<String,Object>();
-                    taskMap.put(locationNotification, isChecked);
+                    userSettings.put(locationNotification, isChecked);
 
                     locationId = IsraWindConsts.locationMap.get(locationNotification).toString();
+
+                    writeSettings();
                     if(isChecked)
                     {
                         FirebaseMessaging.getInstance().subscribeToTopic(locationId)
@@ -173,7 +250,6 @@ public class ScrollingActivityModifyNotifications extends AppCompatActivity {
                                     String msg;
                                     if(task.isSuccessful())
                                     {
-                                        locationRef.updateChildren(taskMap);
                                         msg = "Subscribed to: "+locationNotification;
                                     }
                                     else
@@ -194,7 +270,6 @@ public class ScrollingActivityModifyNotifications extends AppCompatActivity {
                                     String msg;
                                     if(task.isSuccessful())
                                     {
-                                        locationRef.updateChildren(taskMap);
                                         msg = "Unsubscribed from: "+locationNotification;
                                     }
 
